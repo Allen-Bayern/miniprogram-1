582676eb811109b37e812c77eb9461d1cbe625a8
zhaozhengyang_dxm <zhaozhengyang@duxiaoman.com>
2023-03-10 20:14:58 +0800

    feat: ini

---
+++ custom-tab-bar/index.js
@@ -0,0 +1,35 @@
+Component({
+  data: {
+    selected: 0,
+    list: [{
+      pagePath: '/pages/logs/logs',
+      text: 'Logs',
+      iconPath: '/static/tabbar/log.jpg',
+      selectedIconPath: '/static/tabbar/log-selected.jpg'
+    },
+    {
+      pagePath: '/pages/index/index',
+      text: '主页',
+      iconPath: '/static/tabbar/home.jpg',
+      selectedIconPath: '/static/tabbar/home-selected.jpg'
+    },
+    {
+      pagePath: '/pages/wxml/index',
+      text: 'WXML',
+      iconPath: '/static/tabbar/wxml.jpg',
+      selectedIconPath: '/static/tabbar/wxml-selected.jpg'
+    }]
+  },
+  methods: {
+    handleClick(e) {
+      var path = e.currentTarget.dataset.path;
+      var index = e.currentTarget.dataset.index;
+      wx.switchTab({
+        url: path,
+      });
+      this.setData({
+        selected: index
+      });
+    }
+  }
+});
